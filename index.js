3080d4ab34e85825a8c23924a0db2b9618cefb7d
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-11-04 01:45:14 +0600

    retry

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(express.json());
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('running the server with' + port)
-})
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.usnai.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,6 +30,10 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('running the server with' + port)
+})
 
 
 app.listen(port, () => {
